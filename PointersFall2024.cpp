2c8fb1a64c0ad0f3f1cead14a65deef25f78d2ab
Kamel R <kamel.r@wayne.edu>
2024-09-23 18:27:32 -0400

    Add project files.

---
+++ PointersFall2024.cpp
@@ -0,0 +1,49 @@
+// PointersFall2024.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std; 
+int main()
+{
+	int y = 5;
+	int* x = &y;
+
+	//cout << y << endl;
+	cout << x << endl; 
+	//cout << &y << endl;
+	//cout << *x << endl;
+
+	x++;
+	cout << x << endl;
+
+	int size = 5;
+	//int ar1[size];
+
+	int* ar1 = new int[size]; 
+
+	for (int i = 0; i < size; i++)
+		ar1[i] = i;
+
+	for (int i = 0; i < size; i++)
+		cout << ar1[i] << "-" << &ar1[i]<<endl;
+
+	while (true)
+	{
+		delete[] ar1;
+		ar1 = new int[size];
+	}
+
+	int* ptr1 = new int;
+	delete ptr1; // delete a single dynamic memory alloction 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
